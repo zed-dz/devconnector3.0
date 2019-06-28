bae5c8146ece2629c9a2f16c4c14054822161a3a
Zed-M <fsociety.2017@protonmail.com>
2019-06-28 22:23:03 +0100

    fixing the mongodb url

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -22,5 +22,3 @@ export const DELETE_POST = 'DELETE_POST';
 export const ADD_POST = 'ADD_POST';
 export const ADD_COMMENT = 'ADD_COMMENT';
 export const REMOVE_COMMENT = 'REMOVE_COMMENT';
-export const SEARCH_PROFILES = 'SEARCH_PROFILES';
-export const SEARCH_POSTS = 'SEARCH_POSTS';
